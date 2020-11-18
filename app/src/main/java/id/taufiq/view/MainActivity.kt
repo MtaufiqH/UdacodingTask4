38fd0d2f8eb22f5d47926a2faf86e3fc342dfcf7
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2020-11-19 00:09:02 +0800

    adding bottom sheet logic

--- app/src/main/java/id/taufiq/view/MainActivity.kt
+++ app/src/main/java/id/taufiq/view/MainActivity.kt
@@ -1,13 +1,31 @@
 package id.taufiq.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import id.taufiq.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+
+        btn_add_visitor.setOnClickListener {
+            val state =
+                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED)
+                    BottomSheetBehavior.STATE_COLLAPSED
+                else
+                    BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetBehavior.state = state
+
+        }
     }
 }
